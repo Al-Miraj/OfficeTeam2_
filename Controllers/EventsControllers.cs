fa7e81cace685c2c72db78c5881817370cae70b9
Hasan Cakir <hacakir@145-137-59-212.wlan.hro.nl>
2024-10-08 12:50:37 +0200

    Added GetEvents method to EventsController, reading data from Events.json

--- Controllers/EventsControllers.cs
+++ Controllers/EventsControllers.cs
@@ -1,6 +1,8 @@
 using Microsoft.AspNetCore.Mvc;
-using OfficeTeam2_.Models;
+using System.IO;
+using System.Text.Json;
 using System.Collections.Generic;
+using OfficeTeam2_.Models; // Make sure the namespace matches your Event model
 
 namespace OfficeTeam2_.Controllers
 {
@@ -8,37 +10,24 @@ namespace OfficeTeam2_.Controllers
     [Route("api/[controller]")]
     public class EventsController : ControllerBase
     {
-        private static List<Event> events = new List<Event>
-        {
-            new Event 
-            { 
-                Id = 1, 
-                Title = "Event 1", 
-                Description = "Description of Event 1", 
-                Date = DateTime.Now, 
-                StartTime = new TimeSpan(14, 0, 0), // 2 PM
-                EndTime = new TimeSpan(16, 0, 0), // 4 PM
-                Location = "Location 1",
-                Reviews = new List<string> { "Great event!" },
-                Attendees = new List<string> { "Attendee 1", "Attendee 2" }
-            },
-            new Event 
-            { 
-                Id = 2, 
-                Title = "Event 2", 
-                Description = "Description of Event 2", 
-                Date = DateTime.Now.AddDays(1), // Tomorrow
-                StartTime = new TimeSpan(10, 0, 0), // 10 AM
-                EndTime = new TimeSpan(12, 0, 0), // 12 PM
-                Location = "Location 2",
-                Reviews = new List<string> { "Had a great time!" },
-                Attendees = new List<string> { "Attendee 3", "Attendee 4" }
-            }
-        };
+        private readonly string jsonFilePath = Path.Combine(Directory.GetCurrentDirectory(), "Events.json");
 
         [HttpGet]
-        public ActionResult<List<Event>> GetEvents()
+        public IActionResult GetEvents()
         {
+            // Ensure the file exists
+            if (!System.IO.File.Exists(jsonFilePath))
+            {
+                return NotFound("Events.json file not found.");
+            }
+
+            // Read the JSON file contents
+            var jsonContent = System.IO.File.ReadAllText(jsonFilePath);
+            Console.WriteLine(jsonContent); // Debugging: output to verify content
+
+            // Deserialize the content into a list of Event objects
+            var events = JsonSerializer.Deserialize<List<Event>>(jsonContent);
+
             return Ok(events);
         }
     }
--- Models/Event.cs
+++ Models/Event.cs
@@ -3,13 +3,9 @@ namespace OfficeTeam2_.Models
     public class Event
     {
         public int Id { get; set; }
-        public string Title { get; set; }
-        public string Description { get; set; }
-        public DateTime Date { get; set; }
-        public TimeSpan StartTime { get; set; }
-        public TimeSpan EndTime { get; set; }
+        public string Name { get; set; }
+        public string Date { get; set; }
         public string Location { get; set; }
-        public List<string> Reviews { get; set; } = new List<string>();
-        public List<string> Attendees { get; set; } = new List<string>();
+        public List<string> Attendees { get; set; }
     }
 }
