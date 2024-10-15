e55c9cae8dc805706ec4d252e5832c3ac02dbbc7
jvp010 <121518493+jvp010@users.noreply.github.com>
2024-10-15 19:02:29 +0200

    Revert "Reworked log in"

--- Services/ILoginService.cs
+++
@@ -1,8 +0,0 @@
-public interface ILoginService
-{
-    bool Login(Account account);
-    bool CheckSession(string username);
-    // Account GetLoggedInUser(string username);
-    void LogOut(string username);
-    bool Register(Account account);
-}
